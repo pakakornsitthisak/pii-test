80c2193cdd1673ec96e812d36c1a4f46c4cc314d
pakakorn <pakakornsitthisak@DESKTOP-RVQFRPC.localdomain>
2023-06-23 16:29:58 +0700

    second commit

--- app/controllers/v1/UserController.scala
+++ app/controllers/v1/UserController.scala
@@ -1,22 +1,15 @@
 package controllers.v1
 
-import models.BookingTablesPayload
-import play.api.libs.json.Json
 import play.api.mvc._
-import services.BookingService
-import play.api.libs.json._
-
+import services.UserService
 import javax.inject._
-import serializers.v1.ObjectDeserializer._
-import serializers.v1.ObjectSerializer._
-
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class BookingController @Inject()(bookingService: BookingService, val controllerComponents: ControllerComponents) extends BaseController {
+class UserController @Inject()(userService: UserService, val controllerComponents: ControllerComponents) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
--- app/serializers/v1/ObjectSerializer.scala
+++ app/serializers/v1/ObjectSerializer.scala
@@ -1,41 +1,5 @@
 package serializers.v1
 
-import models.{Booking, BookingResult, CancelBookingResult, InitResult}
-import play.api.libs.json.{JsObject, Json, Writes}
-
 object ObjectSerializer {
-
-  implicit val bookingResultJsonWrites = new Writes[BookingResult] {
-    def writes(result: BookingResult): JsObject = Json.obj(
-      "success" -> result.success,
-      "message" -> result.message,
-      "booking_id" -> result.bookingId,
-      "number_of_booked_tables" -> result.numberOfBookedTables,
-      "number_of_remaining_tables" -> result.numberOfRemainingTables,
-    )
-  }
-
-  implicit val cancelNookingResultJsonWrites = new Writes[CancelBookingResult] {
-    def writes(result: CancelBookingResult): JsObject = Json.obj(
-      "success" -> result.success,
-      "message" -> result.message,
-      "number_of_freed_tables" -> result.numberOfFreedTables,
-      "number_of_remaining_tables" -> result.numberOfRemainingTables,
-    )
-  }
-
-  implicit val initResultJsonWrites = new Writes[InitResult] {
-    def writes(result: InitResult): JsObject = Json.obj(
-      "success" -> result.success,
-      "message" -> result.message,
-    )
-  }
-
-  implicit val bookingJsonWrites = new Writes[Booking] {
-    def writes(result: Booking): JsObject = Json.obj(
-      "id" -> result.id,
-      "tables" -> result.tables,
-    )
-  }
 
 }
